e27fe8b65c38aac9d42b02200565969899cd6714
CheeseOnGithub <gamingman32@protonmail.com>
2022-03-20 10:53:34 +0100

    ANOTHER FUCKING FIX FOR MY SHIT CODING

--- main.lua
+++ main.lua
@@ -2,7 +2,8 @@ local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Aika
 local window = library:CreateWindow("shimter hub") -- Creates the window
 local Main = window:CreateFolder("Main") -- Creates the folder(U will put here your buttons,etc)
 local weaponMod = require(game:GetService("Players").LocalPlayer.Backpack.HandGun.Setting)
-local Mods = window:CreateFolder("Mods")
+local Mods = window:CreateFolder("Pistol Mods")
+local rifle = window:CreateFolder("Rifle Mods")
 
 Main:Button("Anti-cheat bypass",function()
     local oldNamecall = nil
@@ -22,6 +23,7 @@ end)
 
 Main:DestroyGui()
 
+-- [ PISTOL MODS ] --
 
 Mods:Label("Execute every respawn",{
     TextSize = 15; -- Self Explaining
@@ -32,81 +34,120 @@ Mods:Label("Execute every respawn",{
 
 Mods:Toggle("auto reload", function(bool)
     local weaponMod = require(game:GetService("Players").LocalPlayer.Backpack.HandGun.Setting)
-    local rifleMod = require(game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("CombatRifle").Setting)
 
     weaponMod.AutoReload = bool
-    rifleMod.AutoReload = bool
 end)
 
 Mods:Toggle("no damage drop off", function(bool)
     local weaponMod = require(game:GetService("Players").LocalPlayer.Backpack.HandGun.Setting)
-    local rifleMod = require(game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("CombatRifle").Setting)
 
 
     weaponMod.DamageDropOffEnabled = bool
-    rifleMod.DamageDropOffEnabled = bool
 end)
 
 Mods:Button("full-auto", function()
     local weaponMod = require(game:GetService("Players").LocalPlayer.Backpack.HandGun.Setting)
-    local rifleMod = require(game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("CombatRifle").Setting)
 
     weaponMod.Auto = true
-    rifleMod.Auto = true
 end)
 
 Mods:Button("inf damage", function()
     local weaponMod = require(game:GetService("Players").LocalPlayer.Backpack.HandGun.Setting)
-    local rifleMod = require(game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("CombatRifle").Setting)
 
     weaponMod.BaseDamage = 9e9
-    rifleMod.BaseDamage = 9e9
 end)
 
 Mods:Button("inf ammo", function()
     local weaponMod = require(game:GetService("Players").LocalPlayer.Backpack.HandGun.Setting)
-    local rifleMod = require(game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("CombatRifle").Setting)
 
     weaponMod.Ammo = 9e9
     weaponMod.MaxAmmo = 9e9
     weaponMod.AmmoPerMag = 9e9
     weaponMod.LimitedAmmoEnabled = false
+end)
+
+Mods:Button("inf range", function()
+    local weaponMod = require(game:GetService("Players").LocalPlayer.Backpack.HandGun.Setting)
+
+    weaponMod.Range = 9e9
+end)
+
+Mods:Button("no recoil", function()
+    local weaponMod = require(game:GetService("Players").LocalPlayer.Backpack.HandGun.Setting)
+
+    weaponMod.Recoil = 0
+end)
+
+Mods:Button("no spread", function()
+    local weaponMod = require(game:GetService("Players").LocalPlayer.Backpack.HandGun.Setting)
+
+    weaponMod.Spread = 0
+end)
+
+Mods:Button("no firing delay", function()
+    local weaponMod = require(game:GetService("Players").LocalPlayer.Backpack.HandGun.Setting)
+
+    weaponMod.DelayBeforeFiring = 0
+    weaponMod.DelayAfterFiring = 0
+end)
+
+
+
+-- [ RIFLE MODS ] --
+rifle:Button("inf damage", function()
+    local rifleMod = require(game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("CombatRifle").Setting)
+
+    rifleMod.BaseDamage = 9e9
+end)
+
+rifle:Button("inf ammo", function()
+    local rifleMod = require(game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("CombatRifle").Setting)
+
     rifleMod.Ammo = 9e9
     rifleMod.MaxAmmo = 9e9
     rifleMod.AmmoPerMag = 9e9
     rifleMod.LimitedAmmoEnabled = false
 end)
 
-Mods:Button("inf range", function()
-    local weaponMod = require(game:GetService("Players").LocalPlayer.Backpack.HandGun.Setting)
+rifle:Button("inf range", function()
     local rifleMod = require(game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("CombatRifle").Setting)
 
-    weaponMod.Range = 9e9
     rifleMod.Range = 9e9
 end)
 
-Mods:Button("no recoil", function()
-    local weaponMod = require(game:GetService("Players").LocalPlayer.Backpack.HandGun.Setting)
+rifle:Button("no recoil", function()
     local rifleMod = require(game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("CombatRifle").Setting)
 
-    weaponMod.Recoil = 0
     rifleMod.Recoil = 0
 end)
 
-Mods:Button("no spread", function()
-    local weaponMod = require(game:GetService("Players").LocalPlayer.Backpack.HandGun.Setting)
+rifle:Button("no spread", function()
     local rifleMod = require(game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("CombatRifle").Setting)
 
-    weaponMod.Spread = 0
     rifleMod.Spread = 0
 end)
 
-Mods:Button("no firing delay", function()
-    local weaponMod = require(game:GetService("Players").LocalPlayer.Backpack.HandGun.Setting)
+rifle:Button("no firing delay", function()
     local rifleMod = require(game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("CombatRifle").Setting)
 
-    weaponMod.DelayBeforeFiring = 0
-    weaponMod.DelayAfterFiring = 0
     rifleMod.DelayBeforeFiring = 0
     rifleMod.DelayAfterFiring = 0
+end)
+
+rifle:Button("full-auto", function()
+    local rifleMod = require(game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("CombatRifle").Setting)
+
+    rifleMod.Auto = true
+end)
+
+rifle:Toggle("no damage drop off", function(bool)
+    local rifleMod = require(game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("CombatRifle").Setting)
+
+    rifleMod.DamageDropOffEnabled = bool
+end)
+
+rifle:Toggle("auto reload", function(bool)
+    local rifleMod = require(game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("CombatRifle").Setting)
+
+    rifleMod.AutoReload = bool
 end)
